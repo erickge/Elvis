2f28da66a7c041aa8d00cb962d9977ac7e2f76b9
Gary <gary@jgerickson.net>
2025-03-27 14:34:43 -0400

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by Gary Erickson on 3/27/25.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
